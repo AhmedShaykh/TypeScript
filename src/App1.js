a7954dac1fd51c6d52028e5902c5fbd123619023
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2022-06-21 13:21:13 +0500

    It's Time To Push !

--- src/App1.js
+++ src/App1.js
@@ -1,5 +1,15 @@
 console.log('Hello TypeScript');
-function type(b, a) {
-    return b / a;
-}
-console.log(type(4, 2));
+// function type (b :number , a :number) {
+//     return b / a
+// }
+// console.log(type(4,2));
+var firstName = "Ahmed";
+var lastName = "Shaikh";
+var age = 21;
+var isActiveDeveloper = true;
+var working = ["JS", "React", "TS"];
+var fullName = "".concat(firstName, " ").concat(lastName);
+console.log(fullName);
+console.log("Age " + age);
+console.log("Status " + isActiveDeveloper);
+console.log("Working " + working);
--- src/App1.ts
+++ src/App1.ts
@@ -1,7 +1,25 @@
 console.log('Hello TypeScript');
 
-function type (b :number , a:number) {
-    return b / a
-}
+// function type (b :number , a :number) {
+//     return b / a
+// }
 
-console.log(type(4,2));
+// console.log(type(4,2));
+
+let firstName: string = "Ahmed";
+let lastName: string = "Shaikh";
+
+let age: number = 21;
+
+let isActiveDeveloper: boolean = true;
+
+let working: string[] = ["JS" , "React" , "TS"];
+
+let fullName: string = `${firstName} ${lastName}`;
+console.log(fullName);
+
+console.log("Age " + age);
+
+console.log("Status " + isActiveDeveloper);
+
+console.log("Working " + working);
