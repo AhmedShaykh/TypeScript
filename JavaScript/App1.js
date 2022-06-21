b6abe752e2e7021bb9a6575a4fb6ec5f8f359bf8
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2022-06-21 13:49:13 +0500

    It's Time To Push !

--- JavaScript/App1.js
+++ JavaScript/App1.js
@@ -1,14 +1,15 @@
+"use strict";
 console.log('Hello TypeScript');
 function type(b, a) {
     return b / a;
 }
 console.log(type(4, 2));
-var firstName = "Ahmed";
-var lastName = "Shaikh";
-var age = 21;
-var isActiveDeveloper = true;
-var working = ["JS", "React", "TS"];
-var fullName = "".concat(firstName, " ").concat(lastName);
+let firstName = "Ahmed";
+let lastName = "Shaikh";
+let age = 21;
+let isActiveDeveloper = true;
+let working = ["JS", "React", "TS"];
+let fullName = `${firstName} ${lastName}`;
 console.log(fullName);
 console.log("Age " + age);
 console.log("Status " + isActiveDeveloper);
---
+++ JavaScript/App2.js
@@ -0,0 +1,3 @@
+"use strict";
+const isActive = true;
+console.log(isActive);
