9a17a9ceeaad7989b0def1d3c8d3cde763aef300
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2022-07-31 23:14:31 +0500

    It's Time To Push !

--- src/App1.ts
+++ src/App1.ts
@@ -23,3 +23,26 @@ console.log("Age " + age);
 console.log("Status " + isActiveDeveloper);
 
 console.log("Working " + working);
+
+document.write("Hello From TS");
+
+const isActive: boolean = true;
+console.log(isActive);
+
+function isEven(num: number) {
+    return num % 2 === 0;
+}
+
+console.log(isEven(5));
+console.log(isEven(2));
+console.log(isEven(10));
+
+let a: any = 1; // Not Attempt
+a = true;
+a = ["React" , "Next" , "Blockchain"];
+
+console.log(a); // Aviod "Any" Type Because It Work Like JavaScipt;
+
+let ts : [number, string, boolean];
+
+console.log(ts = [4, "AHM X", true]);
