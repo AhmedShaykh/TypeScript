153d1d7905af4720ea55390ae6becc290d24cbf2
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2022-08-01 14:36:27 +0500

    It's Time To Push !

--- src/App2.ts
+++
@@ -1,8 +0,0 @@
-var typescript = document.getElementById("type");
-// typescript.onclick = ts;
-
-function ts() {
-    alert('Successfully Login! 0x29D7d1dd5B6f9C864d9db560D72a247c178aE86B');
-}
-
-ts();
